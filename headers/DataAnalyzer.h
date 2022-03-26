6d32ded8835952827f887e19a90a7f45cc75d9de
Arellano-Jann <jannrellano@nevada.unr.edu>
2022-03-26 12:58:10 -0700

    Added tablemaker file

--- headers/DataAnalyzer.h
+++ headers/DataAnalyzer.h
@@ -1,1 +1,25 @@
 #pragma once
+
+class DataAnalyzer
+{
+private:
+	// generates data for one line
+	int numRunTime;
+	int numComparisons;
+	int numSwaps;
+	int totalRunTime;
+	int totalComparisons;
+	int totalSwaps;
+
+public:
+	DataAnalyzer(/* args */);
+	~DataAnalyzer();
+};
+
+DataAnalyzer::DataAnalyzer(/* args */)
+{
+}
+
+DataAnalyzer::~DataAnalyzer()
+{
+}
---
+++ headers/TableMaker.h
@@ -0,0 +1,25 @@
+#pragma once
+
+class TableMaker
+{
+private:
+	// generate table and graph based on these values
+	int avgRunTime;
+	int avgComparisons;
+	int avgSwaps;
+
+	// use avgRunTime to make a graph
+	// sort between sorted and unsorted graphs w algos
+
+public:
+	TableMaker(/* args */);
+	~TableMaker();
+};
+
+TableMaker::TableMaker(/* args */)
+{
+}
+
+TableMaker::~TableMaker()
+{
+}
