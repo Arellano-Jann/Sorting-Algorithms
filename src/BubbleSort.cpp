c9cc5c2797214f264eafb7e04ee68b20d3d47ba5
Arellano-Jann <jannrellano@nevada.unr.edu>
2022-03-25 23:39:28 -0700

    Finished BubbleSort

--- src/BubbleSort.cpp
+++ src/BubbleSort.cpp
@@ -1,5 +1,4 @@
-
-
+template <typename T>
 BubbleSort::BubbleSort(T *arr, int size)
 	: arr(arr)
 	, size(size)
@@ -8,17 +7,28 @@ BubbleSort::BubbleSort(T *arr, int size)
 { sort();
 }
 
+template <typename T>
 void BubbleSort::sort(){
 	for (int i = 0; i < size-1; i++){
 		int index = 0;
 		for (index; index < size-1; index++){
 			if (arr[index] > arr[index+1]){
-				
+				comparisons++;
+				swap(index, index+1);
 			}
 		}
 	}
 }
 
+template <typename T>
+void BubbleSort::swap(currentIndex, swapIndex){
+	T temp = arr[currentIndex];
+	arr[currentIndex] = arr[swapIndex];
+	arr[swapIndex] = temp;
+	swaps++;
+}
+
+template <typename T>
 BubbleSort::~BubbleSort()
 {
 }
